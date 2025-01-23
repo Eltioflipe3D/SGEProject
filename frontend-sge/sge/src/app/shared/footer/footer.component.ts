6eb4acbac6efbbcbdb4a94114423da9544e2c976
Jaime <jaimefs2005@gmail.com>
2025-01-23 12:06:55 +0100

    initial changes

--- frontend-sge/sge/src/app/shared/footer/footer.component.ts
+++ frontend-sge/sge/src/app/shared/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as config from "package.json";
 
 @Component({
   selector: 'app-footer',
@@ -8,10 +9,14 @@ import { Component, OnInit } from '@angular/core';
 export class FooterComponent implements OnInit {
 
   yyyy: number;
+  name: string
+  ver: string
   constructor() { }
 
   ngOnInit() {
     this.yyyy = new Date().getFullYear();
+    this.ver = config.version;
+    this.name = config.name;
   }
 
 }
